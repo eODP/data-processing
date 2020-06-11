0431891f1eb26cf964f52e156d25d586ea88d5fb
wykhuh <wykhuh@users.noreply.github.com>
2020-06-11 07:39:42 -0700

    fix - update extract_taxon_group_from_filename to work with different file name format

--- scripts/normalize_data.py
+++ scripts/normalize_data.py
@@ -205,14 +205,24 @@ def normalize_columns(old_cols, new_col, all_cols):
 
 
 def extract_taxon_group_from_filename(filename):
+    filename = re.sub("-+", "_", filename)
+    filename = re.sub(" +", "_", filename)
+
     filename_parts = re.search(
         # matches 123-U1234A-taxon-group or 123-U1234A-taxongroup
-        "^[0-9]{3}[-_]+U[0-9]{4}[a-zA-Z][-_]+([a-zA-Z]+([-_ ][a-zA-Z]+)?)([_-]\d)?\.csv",
+        "^[0-9]{3}_+U[0-9]{4}[a-zA-Z]_+([a-zA-Z]+(_[a-zA-Z]+)?)(_\d)?_?\.csv",
         filename,
     )
 
+    if filename_parts is None:
+        filename_parts = re.search(
+            # matches 123-taxon-group-U1234A- or 123-taxongroup-U1234A
+            "^[0-9]{3}_+([a-zA-Z]+(_[a-zA-Z]+)?)_+U[0-9]{4}[a-zA-Z](_\d)?_?\.csv",
+            filename,
+        )
+
     if filename_parts is not None:
-        return filename_parts.groups()[0].lower().replace("-", "_").replace(" ", "_")
+        return filename_parts.groups()[0].lower()
     else:
         raise ValueError("Cannot extract taxon group.")
 
--- tests/test_normalize_data.py
+++ tests/test_normalize_data.py
@@ -784,6 +784,65 @@ class TestExtractTaxonGroupFromFilename:
 
         assert extract_taxon_group_from_filename(file) == "taxon_group"
 
+    def test_replace_one_or_more_spaces_with_one_underscore(self):
+        file1 = "123 U1234A taxon group .csv"
+        file2 = "123  U1234A  taxon  group  .csv"
+
+        assert extract_taxon_group_from_filename(file1) == "taxon_group"
+        assert extract_taxon_group_from_filename(file2) == "taxon_group"
+
+    def test_replace_one_or_more_dashes_with_one_underscore(self):
+        file1 = "123-U1234A-taxon-group.csv"
+        file2 = "123--U1234A--taxon--group.csv"
+
+        assert extract_taxon_group_from_filename(file1) == "taxon_group"
+        assert extract_taxon_group_from_filename(file2) == "taxon_group"
+
+    def test_v2_returns_taxon_group_from_filename(self):
+        file = "123_taxon_U1234A.csv"
+
+        assert extract_taxon_group_from_filename(file) == "taxon"
+
+    def test_v2_works_with_hyphens_and_underscores(self):
+        file = "123_taxon-U1234A.csv"
+
+        assert extract_taxon_group_from_filename(file) == "taxon"
+
+    def test_v2_ignores_numbers_after_taxon(self):
+        file1 = "123_taxon-U1234A-1.csv"
+        file2 = "123_taxon-U1234A_1.csv"
+
+        assert extract_taxon_group_from_filename(file1) == "taxon"
+        assert extract_taxon_group_from_filename(file2) == "taxon"
+
+    def test_v2_returns_underscore_version_of_two_part_taxon_names(self):
+        file1 = "123_taxon_group-U1234A.csv"
+        file2 = "123_taxon-group-U1234A.csv"
+        file3 = "123_taxon group-U1234A.csv"
+
+        assert extract_taxon_group_from_filename(file1) == "taxon_group"
+        assert extract_taxon_group_from_filename(file2) == "taxon_group"
+        assert extract_taxon_group_from_filename(file3) == "taxon_group"
+
+    def test_v2_returns_lowercase_taxon_group_from_file(self):
+        file = "123_Taxon_Group_U1234A.csv"
+
+        assert extract_taxon_group_from_filename(file) == "taxon_group"
+
+    def test_v2_replace_one_or_more_spaces_with_one_underscore(self):
+        file1 = "123 taxon group U1234A.csv"
+        file2 = "123  taxon  group  U1234A  .csv"
+
+        assert extract_taxon_group_from_filename(file1) == "taxon_group"
+        assert extract_taxon_group_from_filename(file2) == "taxon_group"
+
+    def test_v2_replace_one_or_more_dashes_with_one_underscore(self):
+        file1 = "123-taxon-group-U1234A.csv"
+        file2 = "123--taxon--group--U1234A.csv"
+
+        assert extract_taxon_group_from_filename(file1) == "taxon_group"
+        assert extract_taxon_group_from_filename(file2) == "taxon_group"
+
 
 class TestCompareDuplicateColumns:
     def test_returns_True_if_duplicate_columns_have_same_value(self):
