f9e14020b73bd2a5d0ac75093ae39430c68c61b0
wykhuh <wykhuh@users.noreply.github.com>
2021-04-20 08:58:19 -0700

    add notebooks to QA NOAA files

---
+++ scripts/normalize_noaa_files.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import pandas as pd
+
+
+def unique_filenames(paths):
+    """Find all the unique filenames for a list of paths"""
+    index = filename_index(paths[0])
+
+    files = set()
+    for path in paths:
+        filename = Path(path).parts[index]
+        files.add(filename)
+
+    return files
+
+def unique_columns(paths):
+    """Find all the unique column names for a list of paths"""
+    columns = set()
+    for path in paths:
+        df = pd.read_csv(path, nrows=1)
+        columns.update(df.columns)
+
+    return columns
+
+def filename_index(path):
+    """Find the index for the filename for a given path string"""
+    return len(Path(path).parts) - 1
+
+def format_filepaths_set(data, type):
+    """Convert a set of filepaths into a string"""
+    if data[type]:
+        value = ','.join(list(data[type]))
+    else:
+        value = ''
+
+    return value
