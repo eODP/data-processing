daaa2eb2e2828d4ffc242668f5accbb2ee135c67
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-07-30 12:17:52 -0700

    update LIMS scripts

--- scripts/normalize_data.py
+++ scripts/normalize_data.py
@@ -3,29 +3,38 @@ import os
 import numpy as np
 import pandas as pd
 
-# HACK: (@)? are meaningless matches so each regex has 7 capture groups
+import normalize_taxa as nt
+
+# HACK: (@)? are meaningless matches so each regex has 8 capture groups
 # (1)-(U1)(A)
 # (Exp)-(Site)(Hole)
-sample_hole_regex = r"(^\d+)-(U\d+)([a-zA-Z])(@)?(@)?(@)?(@)?$"
+sample_hole_regex = r"(^\d+)-(U\d+)([a-zA-Z])(@)?(@)?(@)?(@)?(@)?$"
 # (1)-(U1)(A)-(1)
 # (Exp)-(Site)(Hole)-(Core)
-sample_core_regex = r"(^\d+)-(U\d+)([a-zA-Z])-(\d+)(@)?(@)?(@)?$"
+sample_core_regex = r"(^\d+)-(U\d+)([a-zA-Z])-(\d+)(@)?(@)?(@)?(@)?$"
 # (1)-(U1)(A)-(1)(A)
 # (Exp)-(Site)(Hole)-(Core)(Type)
-sample_type_regex = r"(^\d+)-(U\d+)([a-zA-Z])-(\d+)([a-zA-Z])(@)?(@)?$"
+sample_type_regex = r"(^\d+)-(U\d+)([a-zA-Z])-(\d+)([a-zA-Z])(@)?(@)?(@)?$"
 # (1)-(U1)(A)-(1)(A)-(1)
 # (1)-(U1)(A)-(1)(A)-(A)
 # (Exp)-(Site)(Hole)-(Core)(Type)-(Section)
-sample_sect_regex = r"(^\d+)-(U\d+)([a-zA-Z])-(\d+)([a-zA-Z])-(\w+)(@)?$"
+sample_sect_regex = r"(^\d+)-(U\d+)([a-zA-Z])-(\d+)([a-zA-Z])-(\w+)(@)?(@)?$"
 # (1)-(U1)(A)-(1)(A)-(1)-(1)
 # (1)-(U1)(A)-(1)(A)-(A)-(A)
 # (Exp)-(Site)(Hole)-(Core)(Type)-(Section)-(AW)
-sample_aw_regex = r"(^\d+)-(U\d+)([a-zA-Z])-(\d+)([a-zA-Z])-(\w+)-(\w+)"
+sample_aw_regex = r"(^\d+)-(U\d+)([a-zA-Z])-(\d+)([a-zA-Z])-(\w+)-(\w+)(@)?$"
+# (1)-(U1)(A)-(1)(A)-(1)-(1)-(1)
+# (1)-(U1)(A)-(1)(A)-(A)-(A)-(A)
+# (1)-(U1)(A)-(1)(A)-(A)-(A(A))-(A)
+# (Exp)-(Site)(Hole)-(Core)(Type)-(Section)-(AW)-(Extra)
+sample_extra_regex = (
+    r"(^\d+)-(U\d+)([a-zA-Z])-(\d+)([a-zA-Z])-(\w+)-(\w+(\([\w\-]+\))?)(.*?)$"
+)
 # (1)-(U1)(A)-(1)-(1)-(1)
 # (1)-(U1)(A)-(1)-(A)-(A)
 # (Exp)-(Site)(Hole)-(Core)-(Section)-(AW)
-sample_no_type_aw_regex = r"(^\d+)-(U\d+)([a-zA-Z])-(\d+)(@)?-(\w+)-(\w+)"
-invalid_sample_regex = r"(-)?(-)?(-)?(-)?(-)?(-)?(-)?"
+sample_no_type_aw_regex = r"(^\d+)-(U\d+)([a-zA-Z])-(\d+)(@)?-(\w+)-(\w+)(@)?"
+invalid_sample_regex = r"(-)?(-)?(-)?(-)?(-)?(-)?(-)?(-)?"
 
 
 def tablerize(word):
@@ -52,29 +61,32 @@ def create_directory(newpath):
         os.makedirs(newpath)
 
 
-def normalize_sample_col(df):
+def add_sample_col(df):
     if "Sample" in df.columns:
         pass
-    elif "Label ID" in df.columns:
-        cols = {"Label ID": "Sample"}
-        df.rename(columns=cols, inplace=True)
     else:
         create_sample_name(df)
 
 
 def create_sample_name_for_row(row, columns):
+    if len(row) == 0:
+        return np.nan
+
     name = (
         f"{row['Exp']}-{row['Site']}{row['Hole']}-"
         f"{row['Core']}{row['Type']}-{row['Section']}-{row['A/W']}"
     )
 
-    extra = "Extra Sample ID Data"
-    if extra in columns and row[extra] is not None and row[extra] is not np.NaN:
-        if row["A/W"] == "PAL":
-            name = name + "-" + row[extra]
-        else:
-            name = name + " " + row[extra]
+    if "Extra Sample ID Data" in columns and isinstance(
+        row["Extra Sample ID Data"], str
+    ):
 
+        if row["A/W"] is not np.nan and row["A/W"].startswith("PAL"):
+            name = name + "-" + row["Extra Sample ID Data"]
+        else:
+            name = name + " " + row["Extra Sample ID Data"]
+
+    name = re.sub("nan", "", name)
     name = re.sub("None", "", name)
     name = re.sub("-{2,}", "-", name)
     return re.sub("-$", "", name)
@@ -84,10 +96,32 @@ def create_sample_name(df):
     """Uses Exp...A/W columns to create a name for a sample"""
     names = {"Exp", "Site", "Hole", "Core", "Type", "Section", "A/W"}
     if names.issubset(df.columns):
-        df["Sample"] = df.apply(
+        # create a series of sample names
+        samples_temp = df.apply(
             lambda row: create_sample_name_for_row(row, df.columns), axis=1
         )
 
+        # look for any column that has sample name values
+        duplicate_columns = []
+        for x in range(df.shape[1]):
+            if samples_temp.equals(df.iloc[:, x]):
+                duplicate_columns.append(df.columns.values[x])
+
+        # if there is one column with sample name values, change the name of
+        # of the column to Sample
+        if len(duplicate_columns) == 1:
+            df.rename(columns={duplicate_columns[0]: "Sample"}, inplace=True)
+        # if there is no columns with sample name values, create a Sample
+        # column and populate the column
+        elif len(duplicate_columns) == 0:
+            df["Sample"] = samples_temp
+        # if there are multiple columns with sample name values, drop the
+        # existing columns, and create a Sample column
+        else:
+            df["Sample"] = samples_temp
+            df.drop(duplicate_columns, axis=1, inplace=True)
+
+        return df
     else:
         raise ValueError("File does not have the expected columns.")
 
@@ -115,14 +149,17 @@ def get_expedition_from_csv(df):
         return expeditions[0]
 
 
-def normalize_expedition_section_cols(df):
-    """Create Exp...Section columns using Sample or Label ID"""
+def add_expedition_aw_cols(df):
+    """Create Exp...A/W columns using Sample"""
+    # check if Exp...A/W columns exist
     # NOTE: There was one file that did not have A/W column
     names = {"Exp", "Site", "Hole", "Core", "Type", "Section"}
     if names.issubset(df.columns):
         pass
+    # convert Sample into Exp...A/W columns
     elif "Sample" in df.columns:
         df = df.join(create_sample_cols(df["Sample"]))
+    # convert Label ID into Exp...A/W columns
     elif "Label ID" in df.columns:
         df = df.join(create_sample_cols(df["Label ID"]))
     else:
@@ -152,8 +189,9 @@ def valid_sample_value(name):
         return True
     elif re.search(sample_aw_regex, name):
         return True
+    elif re.search(sample_extra_regex, name):
+        return True
     else:
-        # print("bad", name)
         return False
 
 
@@ -175,6 +213,8 @@ def extract_sample_parts(name):
         result = re.search(sample_no_type_aw_regex, name)
     elif re.search(sample_aw_regex, name):
         result = re.search(sample_aw_regex, name)
+    elif re.search(sample_extra_regex, name):
+        result = re.search(sample_extra_regex, name)
     else:
         result = re.search(invalid_sample_regex, name)
 
@@ -183,6 +223,7 @@ def extract_sample_parts(name):
 
 def create_sample_cols(series):
     """Extract Exp...A/W info from a panda series"""
+
     df = pd.DataFrame(
         {
             "Exp": [],
@@ -192,11 +233,22 @@ def create_sample_cols(series):
             "Type": [],
             "Section": [],
             "A/W": [],
+            "Extra Sample ID Data": [],
         }
     )
 
     for item in series.to_list():
         parts = extract_sample_parts(item)
+
+        if len(parts) == 9:
+            extra = parts[8]
+        else:
+            extra = parts[7]
+
+        if extra:
+            extra = extra.strip()
+            extra = re.sub("^, -|^-|^,", "", extra).strip()
+
         parts_dict = {
             "Exp": parts[0],
             "Site": parts[1],
@@ -205,6 +257,7 @@ def create_sample_cols(series):
             "Type": parts[4],
             "Section": parts[5],
             "A/W": parts[6],
+            "Extra Sample ID Data": extra,
         }
         df = df.append(parts_dict, ignore_index=True)
 
@@ -312,15 +365,18 @@ def extract_taxon_group_from_filename(filename):
         )
 
     if filename_parts is not None:
-        return filename_parts.groups()[0].lower()
+        taxon_group = filename_parts.groups()[0].lower()
+        return nt.update_taxon_group(taxon_group)
     else:
         raise ValueError("Cannot extract taxon group.")
 
 
 def fetch_unique_column_names(path, columns_set):
     content = pd.read_csv(path)
+    content = content.dropna(axis="columns", how="all")
     content = csv_cleanup(content, path)
-    return columns_set.update(set(content.columns))
+    columns = {col.strip() for col in content.columns if col.strip()}
+    return columns_set.update(columns)
 
 
 def append_set(my_set, regex, all_columns):
@@ -333,7 +389,7 @@ def filter_existing_set(my_set, regex):
 
 def add_missing_columns(path, normalized_columns):
     """Add columns to dataframe so every dataframe has the same columns"""
-    content = pd.read_csv(path)
+    content = pd.read_csv(path, dtype=str)
     columns = list(content.columns)
 
     missing_columns = list(set(normalized_columns) - set(columns))
@@ -348,7 +404,7 @@ def add_missing_columns(path, normalized_columns):
     return changed
 
 
-def check_duplicate_columns(df, filename):
+def check_duplicate_columns(df, file_path, needs_review):
     """
     Pandas do not allow duplicate column names. If there are duplicate columns
     in a csv, pd.read_csv keeps the first appearance of a column name, and
@@ -357,28 +413,26 @@ def check_duplicate_columns(df, filename):
 
     This function checks if duplicate columns have the same values.
     """
+    duplicate_columns = []
+
     for column in df.columns:
         # looks for columns renamed by pandas ("foo bar.1")
         if re.match(".*\.\d+$", column):
             # gets original name of the column ("foo bar")
             original_name = re.sub("\.\d+$", "", column)
             if original_name in df.columns:
-                # compare the values in the duplicate columns
-                compare_duplicate_columns = df[original_name].fillna(0) == df[
-                    column
-                ].fillna(0)
-
-                # determines if the two columns have the same values
-                duplicate_columns_are_equal = compare_duplicate_columns.sum() == len(
-                    compare_duplicate_columns
-                )
-                source = f"{filename}, {original_name}"
-                if duplicate_columns_are_equal:
-                    print(f"{source}: duplicate columns have same values")
-                    return True
+                # check if values are equal
+                if df[original_name].equals(df[column]):
+                    duplicate_columns.append(column)
                 else:
-                    print(f"{source}: duplicate columns have different values")
-                    return False
+                    needs_review.append({"path": file_path, "field": original_name})
+                    print(
+                        f"""{file_path}, {original_name}: duplicate columns
+                        have different values"""
+                    )
+
+    df.drop(duplicate_columns, axis=1, inplace=True)
+    return df
 
 
 def get_taxonomy_columns(columns, skip_columns):
@@ -401,3 +455,70 @@ def taxa_needs_review(string):
         return True
     else:
         return False
+
+
+def get_columns_from_disk(metadata, data_directory):
+    columns_all = set()
+    for file in metadata["path"]:
+        fetch_unique_column_names(f"{data_directory}/{file}", columns_all)
+
+    return columns_all
+
+
+def get_columns_from_file_or_disk(columns_file, metadata, data_directory, column_type):
+    # read existing csv
+    if os.path.isfile(columns_file):
+        cols_df = pd.read_csv(columns_file)
+        # get columns for column_type from csv
+        if column_type in cols_df["type"].unique():
+            columns_all = set(cols_df[cols_df["type"] == column_type]["column"])
+        # get columns from disk and append csv
+        else:
+            columns_all = get_columns_from_disk(metadata, data_directory)
+            new_cols_df = pd.DataFrame(
+                {"column": list(columns_all), "type": column_type}
+            )
+            cols_df.append(new_cols_df, ignore_index=True).to_csv(
+                columns_file, index=False
+            )
+    # get columns from disk and create csv
+    else:
+        columns_all = get_columns_from_disk(metadata, data_directory)
+        new_cols_df = pd.DataFrame({"column": list(columns_all), "type": column_type})
+        new_cols_df.to_csv(columns_file, index=False)
+
+    return columns_all
+
+
+def get_common_columns():
+    return {
+        "Sample",
+        "Labl ID",
+        "Label ID",
+        "Exp",
+        "Site",
+        "Hole",
+        "Core",
+        "Core-Sect",
+        "Type",
+        "Section",
+        "A/W",
+        "Extra Sample ID Data",
+        "Bottom Depth [m]",
+        "Bottom Depth[m] [m]",
+        "Bottom depth [m]",
+        "Bottom [cm]",
+        "Bottom offset [cm]",
+        "Bottom[cm] [cm]",
+        "Top Depth [m]",
+        "Top Depth[m] [m]",
+        "Top depth [m]",
+        "Top [cm]",
+        "Top offset [cm]",
+        "Top[cm] [cm]",
+    }
+
+
+def log_df(df, row_count=5):
+    print(df.shape)
+    print(df.head(row_count))
