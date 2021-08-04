f0d504845fc7c59cb9a7c2d4a77a7360ae891a91
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-08-04 15:24:12 -0700

    add notebook to QA NOAA 101-210 files; fix space delimited files

--- scripts/normalize_noaa_files.py
+++ scripts/normalize_noaa_files.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 import pandas as pd
+import re
 
 
-def unique_filenames(paths):
+def unique_filenames_for_paths(paths):
     """Find all the unique filenames for a list of paths"""
     index = filename_index(paths[0])
 
@@ -14,14 +15,24 @@ def unique_filenames(paths):
     return files
 
 
-def unique_columns(paths):
+def unique_columns_for_paths(paths, sep=","):
     """Find all the unique column names for a list of paths"""
     columns = set()
     for path in paths:
-        df = pd.read_csv(path, nrows=1)
+        df = pd.read_csv(path, nrows=1, sep=sep)
         columns.update(df.columns)
 
     return columns
+
+
+def column_counts_for_paths(paths):
+    """Returns the unique number of columns for a list of paths"""
+    counts = set()
+    for path in paths:
+        df = pd.read_csv(path, nrows=0)
+        counts.add(len(df.columns))
+
+    return counts
 
 
 def filename_index(path):
@@ -37,3 +48,38 @@ def format_filepaths_set(data, type):
         value = ""
 
     return value
+
+
+def qa_files_for_paths(paths, expected_fields, sep=","):
+    results = {
+        "bad_encoding": [],  # does not use utf-8 encoding
+        "bad_tabs": [],  # rows have different number of columns than headers
+        "space_delim": [],  # uses spaces as delimiter
+        "missing_fields": [],  # does not have all the fields
+        "good_files": [],
+        "unnamed_column": []
+    }
+
+    for file in paths:
+        try:
+            df = pd.read_csv(file, delimiter=sep, nrows=3)
+        except UnicodeDecodeError:
+            results["bad_encoding"].append(file)
+            continue
+        except pd.errors.ParserError:
+            results["bad_tabs"].append(file)
+            continue
+
+        expected_fields = {re.sub(" {2,}", " ", field) for field in expected_fields}
+        columns = [re.sub(" {2,}", " ", col) for col in df.columns]
+
+        if len([col for col in columns if col.startswith('Unnamed')]) > 0:
+            results["unnamed_column"].append(file)
+        elif expected_fields.issubset(columns):
+            results["good_files"].append(file)
+        elif len(df.columns) == 1:
+            results["space_delim"].append(file)
+        else:
+            results["missing_fields"].append(file)
+
+    return results
