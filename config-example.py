a62bc4687ca280be9c933775750e37da16da164c
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-08-15 19:35:05 -0700

    add paths to config-example.py

--- config-example.py
+++ config-example.py
@@ -1,4 +1,10 @@
+from pathlib import Path
+
 DB_HOST='localhost'
 DB_DATABASE=''
 DB_USER=''
 DB_PASSWORD=''
+
+PROJECT_DIR = Path(__file__).parent.resolve()
+RAW_DATA_DIR=(PROJECT_DIR / 'raw_data')
+OUTPUT_DIR=(PROJECT_DIR / 'output')
