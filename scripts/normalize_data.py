46511070556569607f1c1339c2dc28121151919e
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2022-02-20 19:10:41 -0600

    add more regex matching for extract_taxon_group_from_filename

--- scripts/normalize_data.py
+++ scripts/normalize_data.py
@@ -277,7 +277,7 @@ def extract_taxon_group_from_filename(filename):
 
     filename_parts = re.search(
         # matches 123-U1234A-taxon-group or 123-U1234A-taxongroup
-        "^[0-9]{3}_+U[0-9]{4}[a-zA-Z]_+([a-zA-Z]+(_[a-zA-Z]+)?)(_\d)?_?\.csv",
+        "^[0-9]{3}_+U[0-9]{4}[a-zA-Z]{0,3}_+([a-zA-Z]+(_[a-zA-Z]+)?)(_\d)?_?\.csv",
         filename,
     )
 
@@ -287,6 +287,14 @@ def extract_taxon_group_from_filename(filename):
             "^[0-9]{3}_+([a-zA-Z]+(_[a-zA-Z]+)?)_+U[0-9]{4}[a-zA-Z](_\d)?_?\.csv",
             filename,
         )
+
+    if filename_parts is None:
+        filename_parts = re.search(
+            # matches 123-taxon-group-U1234A_123_T01_taxon_group
+            "^[0-9]{3}_+U[0-9]{4}[a-zA-Z]{0,3}_[0-9]{3}_T[0-9]{2}_+([a-zA-Z]+(_[a-zA-Z]+)?)(_\d)?_?\.csv",
+            filename,
+        )
+
 
     if filename_parts is not None:
         return filename_parts.groups()[0].lower()
