c3655add957d197bd2a78e9e2bbdf5c376675eff
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2022-04-06 15:07:34 -0500

    March 30 import

--- scripts/pbdb.py
+++ scripts/pbdb.py
@@ -1,4 +1,5 @@
 import requests
+import numpy as np 
 
 PBDB_API = "https://paleobiodb.org/data1.2/"
 PBDB_TAXA_NAME = f"{PBDB_API}taxa/single.json?vocab=pbdb&name="
@@ -34,3 +35,50 @@ def get_parent_taxa(df, parent_id, taxon_rank, round, index, data):
                 fill_taxon(df, index, data, taxon_rank)
 
             return get_parent_taxa(df, parent_id, taxon_rank, round, index, data)
+
+
+def fix_pbdb_id(df, correction_text, correct_id):
+    """look up pbdb data for taxon id. update dateframe with new pdbd"""
+    print(correct_id)
+    columns = [
+        'pbdb_taxon_id',
+        'pbdb_taxon_name', 'pbdb_taxon_rank',
+        'family_taxon_id', 'family_taxon_name', 
+        'order_taxon_id',  'order_taxon_name', 
+        'class_taxon_id', 'class_taxon_name',
+        'phylum_taxon_id', 'phylum_taxon_name', 
+        'kingdom_taxon_id', 'kingdom_taxon_name',
+        'unranked clade_taxon_id', 'unranked clade_taxon_name'
+    ]
+
+
+
+    col = "Corrections to pbdb_taxon_id"
+    url_parent = PBDB_TAXA_ID + str(correct_id)
+    response = requests.get(url_parent)
+    if response.status_code == 200:
+        data = response.json()["records"]
+        if len(data) == 1:
+            for taxa_col in columns:
+                df.loc[df[col] == correction_text, taxa_col] = np.nan
+
+            df.loc[df[col] == correction_text, "pbdb_taxon_name"] = data[0][
+                "taxon_name"
+            ]
+            df.loc[df[col] == correction_text, "pbdb_taxon_rank"] = data[0][
+                "taxon_rank"
+            ]
+            df.loc[df[col] == correction_text, "pbdb_taxon_id"] = correct_id
+
+            for index, row in df[df[col] == correction_text].iterrows():
+                round = 0
+                get_parent_taxa(
+                    df, data[0]["parent_no"], data[0]["taxon_rank"], round, index, None
+                )
+
+        else:
+            raise ValueError("multipe ID found")
+    else:
+        raise ValueError("ID not found")
+
+    df.loc[df[col] == correction_text, "corrected"] = True
